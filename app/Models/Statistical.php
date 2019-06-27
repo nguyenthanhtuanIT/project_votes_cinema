b570563b5e2014b28e042a87baefed6da27d8c46
nguyenthanhtuan15 <nguyenthanhtuan.15it@gmail.com>
2019-06-27 10:14:23 +0700

    fix getModel

--- app/Models/Statistical.php
+++ app/Models/Statistical.php
@@ -18,12 +18,12 @@ class Statistical extends BaseModel
     protected $fillable = ['vote_id', 'films_id', 'amount_votes', 'movie_selected'];
     public function getVote()
     {
-        $name = Vote::where('id', $this->vote_id);
+        $name = Vote::find($this->vote_id);
         return $name->name_vote;
     }
     public function getNameFilms()
     {
-        $name = Films::where('id', $this->films_id);
+        $name = Films::find($this->films_id);
         return $name->name_film;
     }
 }
